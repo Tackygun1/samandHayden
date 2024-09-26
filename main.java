db0d91ee78293f98bb61bc44e58c57a772fd508e
Tackygun1 <hsstreet01@bvsd.org>
2024-09-26 07:37:05 -0600

    9/26

--- main.java
+++ main.java
@@ -1,2 +1,6 @@
 public class main {
+    public static void main(String[] args) {
+        stocks NVIDIA = new stocks("NVIDIA",200);
+        System.out.println(NVIDIA.getValue());
+    }
 }
--- stocks.java
+++ stocks.java
@@ -1,5 +1,34 @@
+
 public class stocks {
     private String name;
     private double value;
-    private
+    private int type; // used to dictate the stock's price trend
+    private double[] listOfvalues;
+    public stocks(String name, double price, int type){
+        this.name = name;
+        this.value = price;
+        this.type = type;
+    }
+    public stocks(String name, double price){
+        // will randomly set the stock type if you want it to be a surprise
+        this.name = name;
+        this.value = price;
+        this.type = (int)(Math.random()*2);
+    }
+    public double getValue(){
+        return value;
+    }
+    public String getName(){
+        return name;
+    }
+    public void changeValue(double newValue){
+        this.value = newValue;
+    }
+    public double updateStock(){
+        if (type == 1){
+
+        }
+        return 0.0;
+    }
+
 }
