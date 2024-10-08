49e3ef60123030dc140396d106a6dd90624c2cbf
Tackygun1 <hsstreet01@bvsd.org>
2024-10-08 14:11:26 -0600

    10/8

--- main.java
+++ main.java
@@ -2,7 +2,17 @@
 import java.util.*;
 public class main {
     public static void main(String[] args) {
-        stocks NVIDIA = new stocks("NVIDIA",200);
+
+        stocks NVIDIA = new stocks("NVIDIA",200, 3);
+        NVIDIA.populateListOfValues(); /// must be the first thing called
         System.out.println(NVIDIA.getValue());
+        NVIDIA.watchPrice();
+        System.out.println(NVIDIA.getValue());
+        System.out.println(NVIDIA.getListOfValuesString());
+        NVIDIA.watchPrice();
+        System.out.println(NVIDIA.getValue());
+        NVIDIA.watchPrice();
+
+
     }
 }
