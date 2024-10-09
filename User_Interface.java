5a1609dd73fe3dab8c689d0819dd9688fce92b80
722Frsh <searveson01@bvsd.org>
2024-10-09 15:30:18 -0600

    BJ almost done 10/9

--- User_Interface.java
+++ User_Interface.java
@@ -2,22 +2,20 @@ import javax.swing.*;
 import java.awt.event.*;
 import java.awt.*;
 
-public class User_Interface{
+public class User_Interface {
 
-    public static void main(String args[]){
+    public class Global {
+        public static double money = 1000.1;
+    }
 
-        double money = 1000.1;
-
-
-
-
+    public static void main(String args[]) {
 
 
         JFrame mainFrame = new JFrame("Main Frame");
 
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        mainFrame.setSize(400,400);
+        mainFrame.setSize(600, 400);
 
         JPanel NPanel = new JPanel();
 
@@ -37,11 +35,10 @@ public class User_Interface{
 
         JPanel SPanel = new JPanel();
 
-        SPanel.add(new JLabel("$"+money));
+        SPanel.add(new JLabel("$" + Global.money));
 
         mainFrame.add(SPanel, BorderLayout.SOUTH);
         mainFrame.add(NPanel, BorderLayout.NORTH);
-
 
 
         buttonPoker.addActionListener(new pokerListener());
@@ -52,17 +49,17 @@ public class User_Interface{
         buttonSlots.addActionListener(new slotsListener());
 
         //mainFrame.setLayout(new BorderLayout());
-        
-        mainFrame.setVisible(true);
 
+        mainFrame.setVisible(true);
 
 
     }
 
-    public static class pokerListener implements ActionListener
-    {
+    public static class pokerListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
+
+            //double money = 1000.1;
 
             JFrame pokerFrame = new JFrame("Poker");
             pokerFrame.setSize(400, 400);
@@ -71,9 +68,9 @@ public class User_Interface{
             JButton buttonCall = new JButton("Call");
             JButton buttonFold = new JButton("Fold");
 
-            JTextField raiseAmount = new JTextField("0000000");
+            JTextField raiseAmount = new JTextField(7);
 
-            JLabel displayMoney = new JLabel("$"+money);
+            JLabel displayMoney = new JLabel("$" + Global.money);
 
             JPanel NPanel = new JPanel();
             JPanel SPanel = new JPanel();
@@ -83,17 +80,18 @@ public class User_Interface{
             NPanel.add(buttonRaise);
             NPanel.add(raiseAmount);//Text field
 
-            SPanel.add();
+            SPanel.add(displayMoney);
 
             pokerFrame.add(NPanel, BorderLayout.NORTH);
+            pokerFrame.add(SPanel, BorderLayout.SOUTH);
 
             pokerFrame.setVisible(true);
 
 
         }
     }
-    public static class stocksListener implements ActionListener
-    {
+
+    public static class stocksListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
 
@@ -109,25 +107,136 @@ public class User_Interface{
 
         }
     }
-    public static class BJListener implements ActionListener
-    {
+
+    public static class BJListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
 
             JFrame BJFrame = new JFrame("Blackjack");
-            BJFrame.setSize(400, 400);
+            BJFrame.setSize(450, 400);
 
-            JButton button2 = new JButton("Test");
+            JButton stayButton = new JButton("Stay");
+            JButton hitButton = new JButton("Hit");
+            JButton splitButton = new JButton("Split");
+            JButton DDButton = new JButton("Double Down");
+            JButton betButton = new JButton("Bet");
 
-            BJFrame.getContentPane().add(button2);
+            JPanel NPanel = new JPanel();
+            JPanel SPanel = new JPanel();
+
+            JTextField betAmountInput = new JTextField(7);
+
+            JLabel BJMoney = new JLabel("Your total savings: $" + Global.money);
+
+            JLabel BJBet = new JLabel("Your current bet: $0");
+
+            NPanel.add(stayButton);
+            NPanel.add(hitButton);
+            NPanel.add(splitButton);
+            NPanel.add(DDButton);
+
+            SPanel.add(BJMoney);
+            SPanel.add(BJBet);
+            SPanel.add(betAmountInput);
+            SPanel.add(betButton);
+
+            class betAmountListener implements ActionListener {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    String betAmountS = (betAmountInput.getText());
+                    int betAmount = Integer.parseInt(betAmountS.trim());
+                    if (betAmount<Global.money) {
+                        Global.money -= betAmount;
+                        BJBet.setText("Your current bet: $" + betAmount);
+                        BJMoney.setText("Your total savings: $" + Global.money);
+                        BJFrame.setVisible(false);
+                        SPanel.add(BJBet);
+                        SPanel.add(BJMoney);
+                        SPanel.add(BJBet);
+                        SPanel.add(betAmountInput);
+                        SPanel.add(betButton);
+                        BJFrame.setVisible(true);
+                    }
+                    else{
+                        JOptionPane.showMessageDialog(null, "Not enough funds");
+                    }
+                }
+            }
+            class splitListener implements ActionListener {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+
+                }
+            }
+            class stayListener implements ActionListener {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    String betAmountS = (betAmountInput.getText());
+                    int betAmount = Integer.parseInt(betAmountS.trim());
+                    if (betAmount<Global.money) {
+                        Global.money -= betAmount;
+                        BJBet.setText("Your current bet: $" + betAmount);
+                        BJMoney.setText("Your total savings: $" + Global.money);
+                        BJFrame.setVisible(false);
+                        SPanel.add(BJBet);
+                        SPanel.add(BJMoney);
+                        SPanel.add(BJBet);
+                        SPanel.add(betAmountInput);
+                        SPanel.add(betButton);
+                        BJFrame.setVisible(true);
+                    }
+                    else{
+                        JOptionPane.showMessageDialog(null, "Not enough funds");
+                    }
+                }
+            }
+            class hitListener implements ActionListener {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+
+                }
+            }
+            class DDListener implements ActionListener {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    String betAmountS = (betAmountInput.getText());
+                    int betAmount = Integer.parseInt(betAmountS.trim());
+                    if (betAmount<Global.money) {
+                        Global.money -= betAmount;
+                        BJBet.setText("Your current bet: $" + betAmount);
+                        BJMoney.setText("Your total savings: $" + Global.money);
+                        BJFrame.setVisible(false);
+                        SPanel.add(BJBet);
+                        SPanel.add(BJMoney);
+                        SPanel.add(BJBet);
+                        SPanel.add(betAmountInput);
+                        SPanel.add(betButton);
+                        BJFrame.setVisible(true);
+                    }
+                    else{
+                        JOptionPane.showMessageDialog(null, "Not enough funds");
+                    }
+                }
+            }
+
+            betButton.addActionListener(new betAmountListener());
+            hitButton.addActionListener(new hitListener());
+            stayButton.addActionListener(new stayListener());
+            splitButton.addActionListener(new splitListener());
+            DDButton.addActionListener(new DDListener());
+
+            BJFrame.add(NPanel, BorderLayout.NORTH);
+            BJFrame.add(SPanel, BorderLayout.SOUTH);
 
             BJFrame.setVisible(true);
 
 
+
         }
     }
-    public static class rouletteListener implements ActionListener
-    {
+
+
+    static class rouletteListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
 
@@ -143,7 +252,8 @@ public class User_Interface{
 
         }
     }
-    public static class crapsListener implements ActionListener {
+
+    static class crapsListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
 
@@ -159,7 +269,8 @@ public class User_Interface{
 
         }
     }
-    public static class slotsListener implements ActionListener {
+
+    static class slotsListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
 
@@ -171,15 +282,8 @@ public class User_Interface{
             slotsFrame.getContentPane().add(button2);
 
             slotsFrame.setVisible(true);
-            }
         }
-
-
-
-
-
-
-
+    }
 
 
 }
